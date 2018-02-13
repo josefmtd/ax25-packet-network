d786c02199f4610bff587dac559d667958435fcb
Josef Matondang <josstemat@gmail.com>
2018-02-13 11:54:50 +0700

    recompile program according beacon

--- client/client-0.1/client.c
+++ client/client-0.1/client.c
@@ -1,6 +1,5 @@
 // Generic Libraries
 
-#include <config.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,12 +10,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
-#ifdef HAVE_NETAX25_AX25_H
 #include <netax25/ax25.h>
-#else
-#include <netax25/kernel_ax25.h>
-#endif
-
 #include <netax25/axlib.h>
 #include <netax25/axconfig.h>
 #include <netax25/daemon.h>
@@ -25,18 +19,19 @@ static int logging = FALSE;
 static int mail = FALSE;
 static int single = FALSE;
 
-#define VERSION = 0.1
+#define VERSION "0.1"
 
 int main(int argc, char *argv[]) {
   struct full_sockaddr_ax25 server_address;
   struct full_sockaddr_ax25 client_address;
-  int server_address_length, client_address length;
+  int server_address_length, client_address_length;
+  int option;
   int interval = 30;
   char address[20], *port, *device, *message, *port_call;
   char *client_call = NULL, *server_call = NULL;
 
-  while ((n = getopt(argc, argv, "d:t:lv"))) != -1) {
-    switch (n) {
+  while ((option = getopt(argc, argv, "d:t:lv")) != -1) {
+    switch (option) {
       case 'd':
         server_call = optarg;
         break;
@@ -65,10 +60,10 @@ int main(int argc, char *argv[]) {
     sprintf(address, "%s %s", client_call, port_call);
   }
   else {
-    strcpy(address, portcall);
+    strcpy(address, port_call);
   }
 
-  if (client_address_length = ax25_aton(address, &client_address)) == -1) {
+  if (client_address_length = ax25_aton(address, &client_address) == -1) {
     fprintf(stderr, "client: unable to convert callsign '%s'\n", address);
     return 1;
   }
